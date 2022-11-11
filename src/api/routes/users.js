3745bc90ae1f4234a919382eed8cf63dd062d533
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-11 11:59:37 +0000

    Write /users endpoints and validation

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -1,6 +1,38 @@
 import express from "express";
-import validator from "express-validator";
+
+import { getUser, getShow, isId } from "../middleware";
+import { User, Show } from "../../db/models";
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+	res.send(await User.findAll());
+});
+
+router.get("/:userId", isId("userId"), getUser(), (req, res) => {
+	res.send(req.user);
+});
+
+router.get(
+	"/:userId/shows",
+	isId("userId"),
+	getUser({
+		include: Show,
+	}),
+	(req, res) => {
+		res.send(req.user.shows);
+	}
+);
+
+router.put(
+	"/:userId/shows/:showId",
+	isId(["userId", "showId"]),
+	getUser(),
+	getShow(),
+	async (req, res) => {
+		await req.user.addShow(req.show);
+		res.sendStatus(202);
+	}
+);
 
 export default router;
