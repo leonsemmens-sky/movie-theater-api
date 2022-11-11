9055c2db2f5ec7fd7f5ea88685de242c352d53cb
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-11 15:10:27 +0000

    Convert endpoints to use new through table

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -1,7 +1,10 @@
 import express from "express";
+import validator from "express-validator";
+const vBody = validator.body;
 
-import { getUser, getShow, isId } from "../middleware";
-import { User, Show } from "../../db/models";
+import { getUser, getShow, isId, error } from "../middleware";
+import { User, Show, Viewing } from "../../db/models";
+import { Op } from "sequelize";
 
 const router = express.Router();
 
@@ -17,10 +20,18 @@ router.get(
 	"/:userId/shows",
 	isId("userId"),
 	getUser({
-		include: Show,
+		include: Viewing,
 	}),
-	(req, res) => {
-		res.send(req.user.shows);
+	async (req, res) => {
+		const viewings = await req.user.getViewings({
+			where: {
+				watched: true,
+			},
+			include: Show,
+		});
+
+		const shows = viewings.map((viewing) => viewing.show);
+		res.send(shows);
 	}
 );
 
@@ -30,7 +41,51 @@ router.put(
 	getUser(),
 	getShow(),
 	async (req, res) => {
-		await req.user.addShow(req.show);
+		await req.user.addShow(req.show, {
+			through: {
+				watched: true,
+			},
+		});
+		res.sendStatus(202);
+	}
+);
+
+router.put(
+	"/:userId/rate/:showId",
+	isId(["userId", "showId"]),
+	getUser(),
+	getShow(),
+	[
+		vBody("rating", "Rating must be a float in the range [0, 5]")
+			.isFloat({
+				min: 0,
+				max: 5,
+			})
+			.customSanitizer((rating) => Number(Number(rating).toFixed(1))),
+		error(400),
+	],
+	async (req, res) => {
+		await req.user.addShow(req.show, {
+			through: {
+				rating: req.body.rating,
+			},
+		});
+
+		const viewings = await req.show.getViewings({
+			where: {
+				[Op.not]: {
+					rating: null,
+				},
+			},
+		});
+
+		const averageRating =
+			viewings.reduce((total, viewing) => total + viewing.rating, 0) /
+			viewings.length;
+		await req.show.update({
+			rating: Number(averageRating.toFixed(1)),
+		});
+
 		res.sendStatus(202);
 	}
 );
