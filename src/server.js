0a33f1f8d180704353d79c60938529045118120d
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-11 11:58:11 +0000

    Add /seed endpoint for resetting db

--- src/server.js
+++ src/server.js
@@ -4,7 +4,15 @@ dotenv.config();
 import app from "./api/app";
 import seed from "./db/config/seed";
 
+app.post("/seed", async (req, res) => {
+	try {
+		await seed();
+		res.sendStatus(200);
+	} catch (err) {
+		res.status(500).send(err);
+	}
+});
+
 app.listen(process.env.PORT, async () => {
-	await seed();
 	console.log(`Listening on port ${process.env.PORT}`);
 });
